ce4149f8d759fea165e5146d87ee25b31cc3696a
Jennifer <125608864+JennGoldring@users.noreply.github.com>
2023-03-15 22:05:00 -0500

    additional small changes

--- script.js
+++ script.js
@@ -67,3 +67,20 @@ window.addEventListener("load", function () {
        <img src="${image}">
     `;
  }
+//  let listedPlanets;
+// // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+// let listedPlanetsResponse;
+// listedPlanetsResponse.then(function (result) {
+//     listedPlanets = result;
+//     console.log(listedPlanets);
+// }).then(function () {
+//     console.log(listedPlanets);
+//     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+// })
+let listedPlanetsResponse = fetch();
+listedPlanetsResponse.then(function (result) {
+let listedPlanets = result;
+let missionTarget = document.getElementById("missionTarget");
+let randomPlanet = pickPlanet(listedPlanets);
+
+});
